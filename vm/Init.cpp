c12fc52386b28e4b518240aa891681f2b45ee53d
flowcoaster2 <flowcoaster2@posteo.de>
2016-08-24 13:37:03 +0200

    fixed valgrind path

--- vm/Init.cpp
+++ vm/Init.cpp
@@ -1816,7 +1816,8 @@ bool dvmInitAfterZygote()
 #ifdef WITH_TAINT_TRACKING
     gDvm.disableNativeTainting = disableNativeTainting;
     if (!disableNativeTainting) {
-    const char* part1 = "/data/local/Inst/bin/valgrind --trace-children=yes --tool=taintgrind /system/bin/tgwrapper  ";
+    const char* part1 = "valgrind --trace-children=yes --tool=taintgrind /system/bin/tgwrapper  ";
+    //const char* part1 = "/data/local/Inst/bin/valgrind --trace-children=yes --tool=taintgrind /system/bin/tgwrapper  ";
     //const char* part1 = "/system/bin/tgwrapper  ";
     pid_t pid = getpid();
     char cpid[50];
